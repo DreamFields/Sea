98751c7cd3207efa75abca12f60fa1694636d49b
ttzztztz <ttzztztz@outlook.com>
2021-10-10 21:31:51 +0800

    up

--- src/pages/teacherTraining/studentList.tsx
+++ src/pages/teacherTraining/studentList.tsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import style from './style.less';
+import { post } from '@/utils/request';
+import { Table } from 'antd';
 
 const Component = (props: any) => {
-  return <></>;
+  const [dataSource, setDataSource] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const res = await post<any>('/v1/teacher/student_list');
+      console.log(res);
+      setDataSource(res);
+    })();
+  }, []);
+
+  const columns: any = [
+    {
+      title: '难度',
+      dataIndex: 'id',
+      key: 'id',
+    },
+    {
+      title: '用户名',
+      dataIndex: 'nickname',
+      key: 'nickname',
+    },
+  ];
+  for (let idx = 0; idx < 5; idx++) {
+    columns.push({
+      title: `问题${idx + 1}作答`,
+      dataIndex: ['question_count', (idx + 1).toString()],
+      key: `question_count_${idx}`,
+    });
+    columns.push({
+      title: `问题${idx + 1}正答`,
+      dataIndex: ['question_correct', (idx + 1).toString()],
+      key: `question_correct_${idx}`,
+    });
+  }
+
+  return <Table dataSource={dataSource} columns={columns} />;
 };
 
 export default Component;
