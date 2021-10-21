cc93bcd73a72a60bdd79559081811764929ce73e
DreamFields <1339611623@qq.com>
2021-10-21 18:30:09 +0800

    服务器配置

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,1 +1,8 @@
-export const SERVICEURL = 'http://114.55.176.95:80';
+/*
+ * @Author: Meng Tian
+ * @Date: 2021-10-18 15:01:45
+ * @Description: Do not edit
+ */
+export const SERVICEURL = 'http://114.55.176.95:80'; // 云服务器
+// export const SERVICEURL = 'https://10.0.82.218:443';// docker服务器
+// export const SERVICEURL = 'http://127.0.0.1:80';// 本机
