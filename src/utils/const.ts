0e16ac9b084648ac911a42d268cbb80eabe5dd3f
DreamFields <1339611623@qq.com>
2021-10-21 19:34:31 +0800

    ok

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,1 +1,10 @@
-export const SERVICEURL = 'http://114.55.176.95:80';
+/*
+ * @Author: Meng Tian
+
+ * @Date: 2021-10-18 15:09:08
+ * @Description: Do not edit
+ */
+// http是80端口，https是443端口
+export const SERVICEURL = 'https://114.55.176.95:443'; // 云服务器
+// export const SERVICEURL = 'https://10.0.82.218:443';// docker服务器
+// export const SERVICEURL = 'http://127.0.0.1:80';// 本机
