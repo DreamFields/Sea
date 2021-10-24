41c5adbe9fd325955c6f515057ed93ec68351232
ttzztztz <ttzztztz@outlook.com>
2021-10-24 18:59:28 +0800

    up

--- src/pages/teacherTraining/StudentKaohe.tsx
+++ src/pages/teacherTraining/StudentKaohe.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import style from './style.less';
+import { Button, Table, Space } from 'antd';
 
 const Index = () => {
-  return <></>;
+  const [data, setData] = useState([
+    {
+      name: 'ttzztztz',
+      chapter1: '10/100',
+      chapter2: '10/100',
+      chapter3: '10/100',
+      chapter4: '10/100',
+      chapter5: '10/100',
+      chapter6: '10/100',
+      chapter7: '10/100',
+      chapter8: '10/100',
+      chapter9: '10/100',
+      chapter10: '10/100',
+    },
+  ]);
+
+  const columns: any[] = [
+    {
+      title: '名称',
+      dataIndex: 'name',
+      key: 'name',
+    },
+  ];
+
+  for (let i = 1; i <= 10; i++) {
+    columns.push({
+      title: `chapter${i}`,
+      dataIndex: `chapter${i}`,
+      key: `chapter${i}`,
+    });
+  }
+
+  columns.push({
+    title: '操作',
+    key: 'action',
+    fixed: 'right',
+    render: (data) => (
+      <Space size="middle">
+        <Button
+          onClick={() => {
+            console.log('detail paper', data);
+          }}
+        >
+          详情
+        </Button>
+      </Space>
+    ),
+  });
+
+  return <Table columns={columns} dataSource={data} />;
 };
 
 export default Index;
--- src/pages/teacherTraining/StudentKaoshi.tsx
+++ src/pages/teacherTraining/StudentKaoshi.tsx
@@ -1,7 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import style from './style.less';
+import { Button, Table, Space } from 'antd';
 
 const Index = () => {
-  return <></>;
+  const [data, setData] = useState([
+    {
+      key: '1',
+      name: '11',
+      avg: '100',
+      startTime: new Date().toString(),
+      endTime: new Date().toString(),
+    },
+  ]);
+
+  const columns = [
+    {
+      title: '试卷名称',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: '均分',
+      dataIndex: 'avg',
+      key: 'avg',
+    },
+    {
+      title: '开始时间',
+      dataIndex: 'startTime',
+      key: 'startTime',
+    },
+    {
+      title: '结束时间',
+      dataIndex: 'endTime',
+      key: 'endTime',
+    },
+    {
+      title: '操作',
+      key: 'action',
+      render: (data) => (
+        <Space size="middle">
+          <Button
+            onClick={() => {
+              console.log('detail paper', data);
+            }}
+          >
+            详情
+          </Button>
+        </Space>
+      ),
+    },
+  ];
+
+  return <Table columns={columns} dataSource={data} />;
 };
 
 export default Index;
