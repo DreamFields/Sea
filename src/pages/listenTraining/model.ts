3c0bf9755a591c1b5db9e94ddad6bb585b1ac9d6
DreamFields <1339611623@qq.com>
2021-11-02 19:41:05 +0800

    答题系统-学生端-考核模块

---
+++ src/pages/listenTraining/model.ts
@@ -0,0 +1,119 @@
+import { message } from 'antd';
+import { Effect } from '@umijs/preset-react';
+import { getQuestionsList, getDiffcult } from './service';
+
+export interface StateType {}
+
+export interface ModelType {
+  namespace: string;
+  state: StateType;
+  effects: {
+    getQuestionList: Effect;
+  };
+  reducers: {};
+
+  subscriptions: {};
+}
+
+const Model = {
+  namespace: 'listenTraining',
+
+  state: {
+    difficultList: {},
+    questionList: [
+      //   {
+      //     "analysis": " ",
+      //     "correct": "A",
+      //     "info_text_content": {
+      //         "A": 1,
+      //         "B": 2,
+      //         "C": 3,
+      //         "D": 4,
+      //         "question_info": "1+1=?"
+      //     },
+      //     "knowledge": " ",
+      //     "pic_url": null,
+      //     "question_id": 1,
+      //     "question_status": 2,
+      //     "sound_url": null,
+      //     "student_answer": null
+      // },
+    ],
+  },
+
+  effects: {
+    *getQuestionList({ payload: { chapter, difficult } }, { call, put }) {
+      const question_list = yield call(getQuestionsList, {
+        chapter,
+        difficult,
+      });
+      console.log(question_list);
+
+      if (question_list) {
+        yield put({
+          type: 'getList',
+          payload: {
+            questionList: question_list,
+          },
+        });
+      } else {
+        message.error('获取题目失败');
+      }
+    },
+
+    *getDiffcultList({}, { call, put }) {
+      console.log(123);
+
+      const difficultList = yield call(getDiffcult);
+      if (difficultList) {
+        yield put({
+          type: 'getDifficult',
+          payload: {
+            difficultList: difficultList,
+          },
+        });
+      }
+    },
+
+    // *submitQuestion({ payload: { id, answer } },{ call, put }) {
+    //   const response = yield call(submit,{ id, answer });
+    //   console.log(response);
+
+    //   if (response) {
+    //     console.log(response);
+    //   } else {
+    //     message.error('提交题目失败');
+    //   }
+    // }
+  },
+
+  reducers: {
+    getList(state, { payload: { questionList } }) {
+      return {
+        ...state,
+        questionList,
+      };
+    },
+    getDifficult(state, { payload: { difficultList } }) {
+      return {
+        ...state,
+        difficultList,
+      };
+    },
+  },
+
+  subscriptions: {
+    setup({ dispatch, history }) {
+      history.listen(({ pathname }) => {
+        if (pathname === '/listenTraining') {
+          dispatch({
+            type: 'getDiffcultList',
+            payload: {},
+          });
+        }
+      });
+    },
+  },
+};
+
+export default Model;
---
+++ src/pages/listenTraining/service.ts
@@ -0,0 +1,30 @@
+import request from '@/utils/request';
+
+//获得某一个章节某个题目的题目信息列表
+export async function getQuestionsList({ chapter, difficult }) {
+  return request(`/v1/student/question_list`, {
+    method: 'POST',
+    data: {
+      chapter,
+      difficult,
+    },
+  });
+}
+
+//获得学生可以选择的各个章节难度列表
+export async function getDiffcult() {
+  return request(`/v1/student/student_difficult`, {
+    method: 'POST',
+  });
+}
+
+//向后端提交某个问题的选项
+// export async function submit({ id, answer }) {
+//   return request(`/v1/student/Student_assessment_submission`,{
+//     method: 'POST',
+//     data:{
+//       id,
+//       answer
+//     }
+//   })
+// }
