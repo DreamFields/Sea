1822370118d83dcc1f96c680236886e0be8eaacc
Assnlif <hrj1125@qq.com>
2021-11-12 17:30:49 +0800

    j

--- src/pages/features/index.jsx
+++ src/pages/features/index.jsx
@@ -127,28 +127,6 @@ const Index = (props) => {
           >
             复位
           </Button>
-          <Button
-            type="primary"
-            style={{ fontSize: 15, float: 'left', marginLeft: '16px' }}
-            onClick={() => {
-              html2canvas(document.querySelector('#capture'), {
-                // 转换为图片
-                useCORS: true, // 解决资源跨域问题
-              }).then((canvas) => {
-                // imgUrl 是图片的 base64格式 代码 png 格式
-                let imgUrl = canvas.toDataURL('image/png');
-                //下面是 下载图片的功能。 不需要不加 注意加 .png
-                const str = imgUrl.replace(/data:image\/png;base64,/, '');
-                const file = b64toBlob(str, 'image/png');
-                const clipboardItemInput = new window.ClipboardItem({
-                  'image/png': file,
-                });
-                window.navigator.clipboard.write([clipboardItemInput]);
-              });
-            }}
-          >
-            复制截图
-          </Button>
         </div>
         <div id="wave-timeline" style={{ marginTop: 20 }}></div>
         <div id="waveform" style={{ backgroundColor: '#3D3D3D' }}>
